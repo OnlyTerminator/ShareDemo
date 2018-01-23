a06eb1e7046a78a67227c7bdba985744e3ebfeb7
xufeng.zhang <xufeng.zhang@moji.com>
2018-01-23 16:45:25 +0800

    修改demo调用

--- app/src/main/java/com/aotuman/demo/MainActivity.java
+++ app/src/main/java/com/aotuman/demo/MainActivity.java
@@ -4,18 +4,23 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
+import com.aotuman.share.CommonUtils;
 import com.aotuman.share.MJThirdShareManager;
 import com.aotuman.share.entity.ShareChannelType;
 import com.aotuman.share.entity.ShareContentConfig;
 import com.aotuman.share.entity.ShareContentType;
 import com.aotuman.share.entity.ShareNewConfig;
 import com.aotuman.share.listener.ShareListener;
+
+import java.io.File;
 
 public class MainActivity extends Activity implements View.OnClickListener,ShareListener {
     private MJThirdShareManager mMjThirdShareManager;
@@ -89,6 +94,7 @@ public class MainActivity extends Activity implements View.OnClickListener,Share
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         //...To-do
+                        Toast.makeText(MainActivity.this,"由于第三方只支持分享本地图片，所以我们也是，如果需要分享网络图片，请先自行下载。",Toast.LENGTH_SHORT).show();
                     }
                 });
         normalDialog.setNegativeButton("本地图片",
@@ -96,6 +102,21 @@ public class MainActivity extends Activity implements View.OnClickListener,Share
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         //...To-do
+                        final String path = CommonUtils.getFilesDir(MainActivity.this, "share").getAbsolutePath() + File.separator + "ATMShareImage.png";
+                        ShareContentConfig picConfig = new ShareContentConfig.Builder("图片分享", "这是一张网络风景图")
+                                .messageContent("这个是短信分享的内容")
+                                .localImagePath(path)
+                                .putShareType(ShareChannelType.WX_FRIEND, ShareContentType.PIC).build();
+                        mMjThirdShareManager.doShare(picConfig,true);
+                        new Thread(){
+                            @Override
+                            public void run() {
+                                super.run();
+                                Bitmap bitmap = BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.share_test);
+                                CommonUtils.writeBitmap(new File(path),bitmap,100,true);
+                                mMjThirdShareManager.prepareSuccess(true);
+                            }
+                        }.start();
                     }
                 });
         // 显示
